576f93e66fd892de3e27bff0663c8bc743e6d38c
ton5040 <65410898+ton5040@users.noreply.github.com>
2022-09-15 14:51:53 -0300

    Create item.py

---
+++ modules/item.py
@@ -0,0 +1,10 @@
+class Item:
+  def __init__(self, nomeunico = "default_item", 
+    nomevisual = "Item Padrão", #tipo = "consumivel", 
+    quantidade = 1, desc = "Descrição Padrão! Você provavelmente não deveria ter esse item."
+    ) -> None:
+    self.nomeunico = nomeunico
+    self.nomevisual = nomevisual
+    self.desc = desc
+    self.quantidade = quantidade
+    #self.tipo = tipo #Listagem de tipos de items: consumivel, armadura (capacete, peitoral, etc.), arma, 
